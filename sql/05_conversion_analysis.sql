70947e29a8b661be156ffc93ca4799168ceed5ee
11382chagar <charlied.garrod@outlook.com>
2026-05-29 08:18:20 +0100

    finalised conversion analysis

--- sql/05_conversion_analysis.sql
+++ sql/05_conversion_analysis.sql
@@ -38,17 +38,75 @@ select
 		else ' Over 70'
 	end as age_bracket,
 	count(m.*) as bracket_population,
-	round(sum(case when a.bulkmail_ad = 1 then m.successfully_converted end) / count(a.bulkmail_ad)::decimal * 100,2)  as bulkmail_conversion,
-	round(sum(case when a.twitter_ad = 1 then m.successfully_converted end) / count(a.twitter_ad)::decimal * 100,2)  as twitter_conversion,
-	round(sum(case when a.instagram_ad = 1 then m.successfully_converted end) / count(a.instagram_ad)::decimal * 100,2)  as instagram_conversion,
-	round(sum(case when a.facebook_ad = 1 then m.successfully_converted end) / count(a.facebook_ad)::decimal * 100,2)  as facebook_conversion,
-	round(sum(case when a.brochure_ad = 1 then m.successfully_converted end) / count(a.brochure_ad)::decimal * 100,2)  as brochure_conversion
+	coalesce(round(sum(case when a.bulkmail_ad = 1 then m.successfully_converted end) / count(a.bulkmail_ad)::decimal * 100,2),0)  as bulkmail_conversion,
+	coalesce(round(sum(case when a.twitter_ad = 1 then m.successfully_converted end) / count(a.twitter_ad)::decimal * 100,2),0)  as twitter_conversion,
+	coalesce(round(sum(case when a.instagram_ad = 1 then m.successfully_converted end) / count(a.instagram_ad)::decimal * 100,2),0)  as instagram_conversion,
+	coalesce(round(sum(case when a.facebook_ad = 1 then m.successfully_converted end) / count(a.facebook_ad)::decimal * 100,2),0)  as facebook_conversion,
+	coalesce(round(sum(case when a.brochure_ad = 1 then m.successfully_converted end) / count(a.brochure_ad)::decimal * 100,2),0)  as brochure_conversion
 from marketing_data m
 left join ad_data a
 on m.id = a.id
 group by 1
 order by 1;
 
+select
+	case
+		when m.age between 20 and 30 then '20-30'
+		when m.age between 31 and 40 then '31-40'
+		when m.age between 41 and 50 then '41-50'
+		when m.age between 51 and 60 then '51-60'
+		when m.age between 61 and 70 then '61-70'
+		else ' Over 70'
+	end as age_bracket,
+	count(m.*) as bracket_population,
+	(coalesce(round(sum(case when a.twitter_ad = 1 then m.successfully_converted end) / count(a.twitter_ad)::decimal * 100,2),0)
+		+ coalesce(round(sum(case when a.instagram_ad = 1 then m.successfully_converted end) / count(a.instagram_ad)::decimal * 100,2),0)
+		+ coalesce(round(sum(case when a.facebook_ad = 1 then m.successfully_converted end) / count(a.facebook_ad)::decimal * 100,2),0)) as digital_ad_conversion,
+	(coalesce(round(sum(case when a.brochure_ad = 1 then m.successfully_converted end) / count(a.brochure_ad)::decimal * 100,2),0) +
+		coalesce(round(sum(case when a.bulkmail_ad = 1 then m.successfully_converted end) / count(a.bulkmail_ad)::decimal * 100,2),0)) as traditional_ad_conversion
+from marketing_data m
+left join ad_data a
+on m.id = a.id
+group by 1
+order by 1;
 
+-- Digital ads dominate across the board. In the world in which we live this might not seem surprising, however what is a shock is that this is true across all age brackets.
+-- The over 70s have a digital ad conversion rate of 50.16% where traditional ads only convert 8.95%. Likewise with the 61-70 age bracket where digital ads convert
+-- 42.41% and traditional ads convert 15.63%. The 20-30 age bracket is where this changes (digital ads 28.57% and traditional ads 42.86%) however there are only 7 customers
+-- in this age bracket. This is possible insight in itself, 2Market either doesnt appeal to the younger generation or the small sample
+-- size suggests it's a potentially high growth demographic. The reality is we have insufficient data to make a recommendation on this demographic specifically.
 
+-- Channel success by marital status
+select
+	m.marital_status,
+	count(m.*) as bracket_population,
+	coalesce(round(sum(case when a.bulkmail_ad = 1 then m.successfully_converted end) / count(a.bulkmail_ad)::decimal * 100,2),0)  as bulkmail_conversion,
+	coalesce(round(sum(case when a.twitter_ad = 1 then m.successfully_converted end) / count(a.twitter_ad)::decimal * 100,2),0)  as twitter_conversion,
+	coalesce(round(sum(case when a.instagram_ad = 1 then m.successfully_converted end) / count(a.instagram_ad)::decimal * 100,2),0)  as instagram_conversion,
+	coalesce(round(sum(case when a.facebook_ad = 1 then m.successfully_converted end) / count(a.facebook_ad)::decimal * 100,2),0)  as facebook_conversion,
+	coalesce(round(sum(case when a.brochure_ad = 1 then m.successfully_converted end) / count(a.brochure_ad)::decimal * 100,2),0)  as brochure_conversion
+from marketing_data m
+left join ad_data a
+on m.id = a.id
+group by 1
+order by 1;
 
+select
+	m.marital_status,
+	count(m.*) as bracket_population,
+	(coalesce(round(sum(case when a.twitter_ad = 1 then m.successfully_converted end) / count(a.twitter_ad)::decimal * 100,2),0)
+		+ coalesce(round(sum(case when a.instagram_ad = 1 then m.successfully_converted end) / count(a.instagram_ad)::decimal * 100,2),0)
+		+ coalesce(round(sum(case when a.facebook_ad = 1 then m.successfully_converted end) / count(a.facebook_ad)::decimal * 100,2),0)) as digital_ad_conversion,
+	(coalesce(round(sum(case when a.brochure_ad = 1 then m.successfully_converted end) / count(a.brochure_ad)::decimal * 100,2),0) +
+		coalesce(round(sum(case when a.bulkmail_ad = 1 then m.successfully_converted end) / count(a.bulkmail_ad)::decimal * 100,2),0)) as traditional_ad_conversion
+from marketing_data m
+left join ad_data a
+on m.id = a.id
+group by 1
+order by 1;
+
+-- Like the age demographic, marital status bares similar results. Digital ads are far more successful across marital statuses with widows 
+-- (57.74% vs 10.96% for traditional ads) being most respondent.
+-- Given the sample sizes, married couples (41.88% vs 12.89%) and unmarried couples (41.92% vs 17.75%) tell the most compelling story, 
+-- that digital ads truly are the most effective ad channels.
+-- Like the 20-30 year olds in the previous query, the Unknown marital statuses have a small sample size and therefore their results are a statistical artefact.
